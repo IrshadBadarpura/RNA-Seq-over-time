c66a68e8e8c95d73ed1521db39a8c2167804c19f
Abdul Wahhab <abdul@10-151-218-74.vlan912.client.wireless.uic.edu>
2020-11-11 23:22:47 -0600

    Calling dropdown function within d3.csv call. Added a working dropdown which searhes my genes names. Also added the call back function.

--- src/main.js
+++ src/main.js
@@ -3,38 +3,58 @@ var day1Dataset = "Day1";
 var day2Dataset = "Day2";
 var day0Data, day1Data, day2Data, day3Data, day4Data, day5Data;
 
-day0Data = loadData(day0Dataset);
-day2Data = loadData(day1Dataset);
-day3Data = loadData(day2Dataset);
-// day4Data = loadData(dataset);
-// day5Data = loadData(dataset);
-// day6Data = loadData(dataset);
+loadData(day0Dataset);
+loadData(day1Dataset);
+loadData(day2Dataset);
+// loadData(dataset);
+// loadData(dataset);
+// loadData(dataset);
 
 function loadData(fileName){
     var url = "../data/" + fileName + ".csv";
 
     var dataArray = [];
+    var geneArray = [];
+    var cellArray = [];
 
     d3.csv(url).then(function(data) {
 
         for(var i = 0; i < data.length; i++){
             var values = Object.values(data[i]);
             var keys = Object.keys(data[i]);
-            var cellName = values[0];
+            var geneName = values[0];
 
             var cellDict = {};
 
             for(var j = 1; j < values.length; j++){
-                cellDict["cell"] = cellName;
-                cellDict["gene"] = keys[j];
+                cellDict["gene"] = geneName;
+                cellDict["cell"] = keys[j];
                 cellDict["expession"] = values[j];
+                
+                if(geneArray.length == 0){
+                    cellArray.push(keys[j]);
+                }
 
                 dataArray.push(cellDict);
                 cellDict = {};
             }
-        }
-    })
 
-    return dataArray;
+            geneArray.push(geneName);
+        }
+
+        createDropdown("ddlGenes", geneArray);
+    });
 }
+
+function createDropdown(id, array){
+    $( "#"+id ).autocomplete({
+        source: array
+    });
+}
+
+$("#ddlGenes").autocomplete({
+    select: function( event, ui ) {
+        console.log(ui.item.value);
+    }
+  });
 
